5cfb63498687e0cff5c7a6ca4242e2fd3b4b5589
Daniel Muñoz Mejía <danielmm1999123@gmail.com>
2024-10-19 15:42:08 -0500

    Initial commit

---
+++ lab3GraphQL/src/main/java/com/udea/lab3GraphQL/Lab3GraphQlApplication.java
@@ -0,0 +1,13 @@
+package com.udea.lab3GraphQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3GraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3GraphQlApplication.class, args);
+	}
+
+}
